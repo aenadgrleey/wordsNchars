4aae187959c643c3b19dd661bbd595046c18fbc3
aenadgrleey <aenadgrleey@protonmail.com>
2023-03-08 16:52:59 +0100

    tuned scription offset and relative size

--- wordsnchars/src/main/java/com/wordsnchars/text_editor/core/custom_spans/ScriptionSpan.kt
+++ wordsnchars/src/main/java/com/wordsnchars/text_editor/core/custom_spans/ScriptionSpan.kt
@@ -2,20 +2,18 @@ package com.wordsnchars.text_editor.core.custom_spans
 
 import android.text.TextPaint
 import android.text.style.RelativeSizeSpan
-import android.text.style.SubscriptSpan
-import android.text.style.SuperscriptSpan
 
 const val noscriptionFlag = 0
 const val subscriptionFlag = 1
 const val superscriptionFlag = 2
 
-class ScriptionSpan(val scriptionType: Int) : RelativeSizeSpan(0.5F) {
+class ScriptionSpan(val scriptionType: Int) : RelativeSizeSpan(0.7F) {
     override fun updateDrawState(textPaint: TextPaint) {
         textPaint.baselineShift += when (scriptionType) {
             subscriptionFlag ->
-                -(textPaint.ascent() / 2).toInt()
+                -(textPaint.ascent() / 4).toInt()
             superscriptionFlag ->
-                (textPaint.ascent() / 2).toInt()
+                (textPaint.ascent() / 3).toInt()
             else -> 0
         }
         if (scriptionType != 0)
@@ -25,9 +23,9 @@ class ScriptionSpan(val scriptionType: Int) : RelativeSizeSpan(0.5F) {
     override fun updateMeasureState(textPaint: TextPaint) {
         textPaint.baselineShift += when (scriptionType) {
             subscriptionFlag ->
-                -(textPaint.ascent() / 2).toInt()
+                -(textPaint.ascent() / 4).toInt()
             superscriptionFlag ->
-                (textPaint.ascent() / 2).toInt()
+                (textPaint.ascent() / 3).toInt()
             else -> 0
         }
         if (scriptionType != 0)
